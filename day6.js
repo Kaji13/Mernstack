71116d2f50ec0a9c7dbaf01f0bdbf264b7ac29f0
kaji <kyromx913@gmail.com>
2026-07-09 18:23:35 +0545

    Add new files

---
+++ day6.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Kasu",
+    age: "19",
+    faculty: "BIT",
+    college: "IIC"
+};
+
+student.address = "Itahari"; 
+student.age = "19";
+delete student.faculty; 
+console.log(student);
