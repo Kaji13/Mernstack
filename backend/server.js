f18f838a27e757882dd4cb80726be9638778c0e1
kaji <kyromx913@gmail.com>
2026-08-26 21:36:32 +0545

    connection of MongoDB atlas

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,6 @@ app.use((err, _req, res, _next) => {
   })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
